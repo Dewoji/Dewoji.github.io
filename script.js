7f2c18711b245d69b1f3c91712951ec3d303b821
Dewoji <HardGamePlayer89@gmail.com>
2025-11-03 00:05:54 +0100

    Added script.js Added style.css Added images Fixed index.html

---
+++ script.js
@@ -0,0 +1,38 @@
+const submitButton = document.getElementById('submitButton');
+
+const popUp = document.getElementById('imagePopUp');
+const popUpImage = document.getElementById('popUpImage');
+
+const images = ['images/qr.png','images/qr2.png', 'images/qr3.png'];
+
+
+function getRandomImage() {
+    const randomNum = Math.floor(Math.random() * images.length);
+    return images[randomNum];
+}
+
+function submitButtonClick() {
+    const image = getRandomImage();
+    popUpImage.src = image;
+    popUp.style.display = 'flex';
+}
+
+window.onclick = function(event) {
+    if(event.target === popUp) {
+        popUp.style.display = 'none';
+    }
+}
+
+submitButton.onclick = async function() {
+    const url = document.getElementById('url').value;
+    try {
+        const response = await fetch(url, {method: 'HEAD'});
+        if(response.ok){
+            submitButtonClick();
+        } else {
+            alert('Invalid URL!');
+        }
+    } catch {
+        alert('URL fetch error!');
+    }
+}
